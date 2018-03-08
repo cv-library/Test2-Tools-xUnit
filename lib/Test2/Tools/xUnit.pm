0cf5937e59decab8148bbc0e7ca397f3306684d6
James Raspass <jraspass@gmail.com>
2018-03-08 12:38:41 +0000

    nits

--- lib/Test2/Tools/xUnit.pm
+++ lib/Test2/Tools/xUnit.pm
@@ -1,19 +1,17 @@
-package Test2::Tools::xUnit;
+package Test2::Tools::xUnit 0.001;
 
 use strict;
 use warnings;
 
 use B;
 use Carp qw/croak/;
-use Test2::Workflow qw/current_build root_build init_root/;
-use Test2::Workflow::Runner();
-use Test2::Workflow::Task::Action();
-
-our $VERSION = '0.01';
+use Test2::Workflow qw/current_build init_root root_build/;
+use Test2::Workflow::Runner;
+use Test2::Workflow::Task::Action;
 
 sub import {
     my $class  = shift;
-    my @caller = caller(0);
+    my @caller = caller;
 
     my $root = init_root(
         $caller[0],
@@ -37,7 +35,7 @@ sub import {
     );
 
     no strict 'refs';
-    *{ $caller[0] . '::MODIFY_CODE_ATTRIBUTES' } = \&handle_attributes;
+    *{"$caller[0]::MODIFY_CODE_ATTRIBUTES"} = \&handle_attributes;
 }
 
 # This gets exported into the caller's namespace as MODIFY_CODE_ATTRIBUTES.
@@ -47,49 +45,47 @@ sub handle_attributes {
     my $name = B::svref_2object($code)->GV->NAME;
     my ( $method, %options );
 
-    for my $attr (@attrs) {
-        if ( $attr eq 'Test' ) {
+    for (@attrs) {
+        if ( $_ eq 'Test' ) {
             $method = 'add_primary';
         }
-        elsif ( $attr eq 'BeforeEach' ) {
+        elsif ( $_ eq 'BeforeEach' ) {
             $method = 'add_primary_setup';
             $options{scaffold} = 1;
         }
-        elsif ( $attr eq 'AfterEach' ) {
+        elsif ( $_ eq 'AfterEach' ) {
             $method = 'add_primary_teardown';
             $options{scaffold} = 1;
         }
-        elsif ( $attr eq 'BeforeAll' ) {
+        elsif ( $_ eq 'BeforeAll' ) {
             $method = 'add_setup';
             $options{scaffold} = 1;
         }
-        elsif ( $attr eq 'AfterAll' ) {
+        elsif ( $_ eq 'AfterAll' ) {
             $method = 'add_teardown';
             $options{scaffold} = 1;
         }
-        elsif ( $attr =~ m/^Skip(?:\((.+)\))?/ ) {
+        elsif ( /^Skip(?:\((.+)\))?/ ) {
             $options{skip} = $1 || $name;
         }
-        elsif ( $attr =~ m/^Todo(?:\((.+)\))?/ ) {
+        elsif ( /^Todo(?:\((.+)\))?/ ) {
             $options{todo} = $1 || $name;
         }
         else {
-            push @unhandled, $attr;
+            push @unhandled, $_;
         }
     }
 
     if ($method) {
-        my ( undef, $filename, $linenum ) = caller 2;
-
         my $task = Test2::Workflow::Task::Action->new(
             code  => $code,
-            frame => [ $pkg, $filename, $linenum ],
+            frame => [ caller 1 ],
             name  => $name,
             %options,
         );
 
         my $current = current_build() || root_build($pkg)
-            or croak "No current workflow build!";
+            or croak 'No current workflow build!';
 
         $current->$method($task);
     }
